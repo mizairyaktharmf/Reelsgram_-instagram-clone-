d85b411b644f2e4d22aabdf2fc397f4347f94545
mizairyaktharmf <mizairyakthar@gmail.com>
2025-09-09 03:50:39 +0400

    created two jsx file

--- Reelsgram/src/Feed.jsx
+++ Reelsgram/src/Feed.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Stories from './Stories'
+import Posts from './Posts'
 
 function Feed() {
   return (
-    <div>Feed</div>
+    <div>
+      <div><Stories /></div>
+      <div><Posts /></div>
+    </div>
   )
 }
 
