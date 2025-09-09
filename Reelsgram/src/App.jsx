192942c8f144627c50133149c3beabe451b332c7
mizairyaktharmf <mizairyakthar@gmail.com>
2025-09-10 00:18:15 +0400

    created and modifed some extra file

--- Reelsgram/src/App.jsx
+++ Reelsgram/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className='d-flex vh-100'>
       <div className='w-20'> <Sidebar/> </div>
-      <div className='bg-info w-50'><Feed /></div>
-      <div className='w-23'><Suggestions /></div>
+      <div className='w-50'><Feed /></div>
+      <div className='w-30'><Suggestions /></div>
     </div>
   )
 }
