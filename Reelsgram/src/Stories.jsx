6a5222de4ef020241705459699047075ba3b0cf2
mizairyaktharmf <mizairyakthar@gmail.com>
2025-09-10 02:42:38 +0400

    created Stories.jsx and add data to db.jason

--- Reelsgram/src/Stories.jsx
+++ Reelsgram/src/Stories.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Stories() {
+
+  const [stories , setStories] = useState([])
+
+  useEffect(()=>{
+        fetch('http://localhost:3000/story')
+        .then((data)=>data.json())
+        .then((data)=>setStories(data)).
+        catch(err=> console.log(err))
+      },[]);
+
+
+
   return (
-    <div className=' stories bg-info'>Stories</div>
+    <div className=' story d-flex'>
+      {stories.length > 0 ? (
+        stories.map((story) =>(
+          <div key={story.id} className='mx-1'>
+            <div className='gradient-border'>
+              <img src={story.user.profile_pic} alt="user profile pic"  className='story-dp rounded-circle'/>
+            </div>
+
+            <p className='text-truncate' style ={{width :"50px"}}>{story.user.username}</p>
+          </div>  
+        ))
+      ) : (
+        <p>Loading Stories</p>
+      )}
+    </div>
   )
 }
 
